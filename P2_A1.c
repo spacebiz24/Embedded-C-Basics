fe93b59ff43a0daac08bb1f80393db2bca281e32
Siddhaanth S Iyer <78657717+spacebiz24@users.noreply.github.com>
2023-02-03 13:23:22 +0530

    Create P2_A1.c

---
+++ P2_A1.c
@@ -0,0 +1,15 @@
+// Invert every bit
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    if (argc > 1)
+    {
+        int value = atoi(argv[1]);
+        printf("Value: %d\n", value);
+        printf("Inverted Value: %d\n", value ^ 0xF);
+    }
+    return 0;
+}
