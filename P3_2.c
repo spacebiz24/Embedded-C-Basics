b75f35c94beef086bf663e20efe66058317bf6f2
Srirangarajan™ <106532953+Eloquencere@users.noreply.github.com>
2023-02-11 20:04:44 +0530

    New programs

---
+++ P3_2.c
@@ -0,0 +1,13 @@
+#include<fcntl.h>
+#include<unistd.h>
+int main()
+{
+	char *c;
+	int fin = open("in.txt", O_RDONLY);
+	int fout = open("out.txt", O_WRONLY | O_CREAT, 0777); // 0777 is octal to give exectue access
+	while (read(fin, &c, 1) == 1)
+		write(fout, &c, 1);
+    close(fin);
+    close(fout);
+	return 0;
+}
