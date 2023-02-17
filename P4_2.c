6b98138ad4b9f01d5f3996cddb7c3ed4b8720ba8
Srirangarajan™ <106532953+Eloquencere@users.noreply.github.com>
2023-02-17 20:22:57 +0530

    Finiding parent and child process IDs

---
+++ P4_2.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/wait.h>
+
+int main(void)
+{
+	char *message;
+	pid_t Creating_Process = fork();
+	
+	switch (Creating_Process)
+	{
+		case -1:
+			printf("Fork Creation Failed\n");
+			exit(1);
+			
+		case 0:
+			message = "Child Executing";
+			break;
+			
+		default:
+			printf("Parent process waiting for child to finish executing\n");
+			wait (NULL);
+			printf("Child process has finished executing\n");
+			message = "Parent Executing";
+			break;
+	}
+	
+	puts(message);
+	printf("Current Process ID is %d\n", getpid());
+	printf("Parent Process ID is %d\n", getppid());
+	
+	return 0;
+}
