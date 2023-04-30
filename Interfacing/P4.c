a5bf51fe3214c713714a8982874390a813d450e9
Srirangarajan™ <106532953+Eloquencere@users.noreply.github.com>
2023-04-30 21:04:24 +0530

    Update P4.c

--- Interfacing/P4.c
+++ Interfacing/P4.c
@@ -35,18 +35,18 @@ int GetScancode()
     return NULL;
 }
 
-int Decode(int ScanCode)
-{
-    int CodeIndex;
-    while (ScanCode != ScanCodeTable[CodeIndex])
-        CodeIndex++;
-    return CodeIndex;
-}
-
 unsigned int SevenSegTable[] = {0x3F /*0*/, 0x06 /*1*/, 0x5B /*2*/, 0x4F /*3*/,
                                 0x66 /*4*/, 0x6D /*5*/, 0x7D /*6*/, 0x07 /*7*/,
                                 0x7F /*8*/, 0x6F /*9*/, 0x77 /*A*/, 0x7C /*B*/,
                                 0x39 /*C*/, 0x5E /*D*/, 0x79 /*E*/, 0x71 /*F*/};
+
+int Display(int ScanCode)
+{
+    int CodeIndex;
+    while (ScanCode != ScanCodeTable[CodeIndex])
+        CodeIndex++;
+    IO0SET = SevenSegTable[CodeIndex)] << 8;
+}
 
 void main()
 {
@@ -60,6 +60,6 @@ void main()
         ScanCode = GetScanCode();
         if (ScanCode == NULL) // Couldn't locate the Key
             continue;
-        IO0SET = SevenSegTable[Decode(ScanCode)] << 8; // display is on P0.8
+        Display(ScanCode);
     }
 }
