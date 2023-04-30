11805b15852785d29213caaa8ee207b658ee9ecd
Srirangarajan™ <106532953+Eloquencere@users.noreply.github.com>
2023-04-30 21:30:38 +0530

    Update P2.c

--- Interfacing/P2.c
+++ Interfacing/P2.c
@@ -3,6 +3,10 @@
 #include "lpc214x.h"
 #include "stdint.h"
 
+unsigned int SevenSegTable[] = {0x3F /*0*/, 0x06 /*1*/, 0x5B /*2*/,
+                                0x4F /*3*/, 0x66 /*4*/, 0x6D /*5*/,
+                                0x7D /*6*/, 0x07 /*7*/, 0x7F /*8*/, 0x6F /*9*/};
+
 void delay_by(unsigned int delayVal)
 {
     for (int i = 0; i <= delayVal; i++);
@@ -16,10 +20,6 @@ void init_LPC()
     IO1DIR = ~0x0; // all GPIO -> output
     IO1SET = ~0x0; // Disabling all displays
 }
-
-unsigned int SevenSegTable[] = {0x3F /*0*/, 0x06 /*1*/, 0x5B /*2*/,
-                                0x4F /*3*/, 0x66 /*4*/, 0x6D /*5*/,
-                                0x7D /*6*/, 0x07 /*7*/, 0x7F /*8*/, 0x6F /*9*/};
 
 void display(unisgned int counterVal)
 {
