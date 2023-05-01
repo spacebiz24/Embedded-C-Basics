9b13ee8ae9d3b053bb4850642036fcb3f63bc37e
Srirangarajan™ <106532953+Eloquencere@users.noreply.github.com>
2023-05-01 08:50:47 +0530

    Create P7.c

---
+++ Interfacing/P7.c
@@ -0,0 +1,47 @@
+#include "lpc214x.h"
+#include "stdint.h"
+#include "string.h"
+
+void delay_by(unsigned int delayVal)
+{
+    for (int i = 0; i <= delayVal; i++);
+}
+
+void init_LPC()
+{
+    PINSEL0 = 0x0; // P0.0,..., P0.15 -> GPIO
+    IO0DIR = 0xF;  // P0.0,..., P0.3 -> output
+    IO0CLR = 0xF;  // outputs -> 0
+}
+
+void ClockwiseRotation(int ActiveCoil)
+{
+    while (ActiveCoil <= 0b1000)
+    {
+        delay_by(10000);
+        ActiveCoil <<= 1;
+    }
+}
+
+void AntiClockwiseRotation(int ActiveCoil)
+{
+    while (ActiveCoil >= 0b0001)
+    {
+        delay_by(10000);
+        ActiveCoil >>= 1;
+    }
+}
+
+void main()
+{
+    init_LPC();
+    int TotalRotations = 550;
+    int ActiveCoil = 0b001;
+    while (1)
+    {
+        for (int Rotation = 0; Rotation <= TotalRotations; Rotation++)
+            ClockwiseRotation(ActiveCoil);
+        for (int Rotation = 0; Rotation <= TotalRotations; Rotation++)
+            AntiClockwiseRotation(ActiveCoil);
+    }
+}
